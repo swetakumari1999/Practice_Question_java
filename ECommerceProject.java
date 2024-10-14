cccfc8348872bc5d154e3093889117a6ba152b27
swetakumari1999 <sweta.kumari@kibalabs.in>
2024-10-14 16:24:33 +0530

    Add files via upload

--- ECommerceProject.java
+++ ECommerceProject.java
@@ -1,5 +1,7 @@
 import java.time.LocalDate;
 import java.util.*;
+import java.util.stream.Collector;
+import java.util.stream.*;
 
 // Class customer order
 class Order {
@@ -29,7 +31,7 @@ class TotalCustomer {
 class FilterOrder {
     public List<TotalCustomer> fetchTop3Customers(List<Order> orders) {
         LocalDate aYearBefore = LocalDate.now().minusYears(1);
-        Map<String, Double> totalCustomers = new HashMap<>();
+       /* Map<String, Double> totalCustomers = new HashMap<>();
 
         // Calculate total amounts and apply discounts
         for (Order order : orders) {
@@ -52,9 +54,25 @@ class FilterOrder {
         customerTotalList.sort((c1, c2) -> Double.compare(c2.totalAmount, c1.totalAmount));
 
         return customerTotalList.size() > 3 ? customerTotalList.subList(0, 3) : customerTotalList;
-    }
-}
+    */
 
+
+        return orders.stream().collect(Collectors.groupingBy(
+                order -> order.customer, Collectors.summingDouble(order -> {
+                    double amount = order.amount;
+                    if(order.date.isBefore(aYearBefore)) {
+                        amount *= 0.9;
+                    }
+                    return amount;
+    })
+    ))
+            .entrySet().stream()
+    .map(entry -> new TotalCustomer(entry.getKey(), entry.getValue()))
+                .sorted((c1, c2) -> Double.compare(c2.totalAmount, c1.totalAmount))
+                    .limit(3) // Get top 3 customers
+                .collect(Collectors.toList());
+}
+}
 // Main class to run the program
 public class ECommerceProject {
     public static void main(String[] args) {
