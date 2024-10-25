9c8520e5a6bd8b10f449ffbac6f885c7ba60176f
swetakumari1999 <sweta.kumari@kibalabs.in>
2024-10-25 17:49:37 +0530

    Add files via upload

---
+++ Basic-Spring-Project/src/test/java/com/spring/someproperties_Spring_Project/SomePropertiesSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.someproperties_Spring_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SomePropertiesSpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ demo/SortAlgorithm.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+public class SortAlgorithm
+{
+
+    public static int[] sort(int[] numbers) {
+
+        return numbers;
+    }
+}
